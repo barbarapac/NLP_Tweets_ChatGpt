aa464b6152bc03c6af32f09a9eef983b0ef9e59f
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2023-05-22 10:48:01 -0300

    fix: corrigindo remoção de stopwords

--- ChatGPT.py
+++ ChatGPT.py
@@ -10,21 +10,23 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 analyzer = SentimentIntensityAnalyzer()
 
-def preprocess_tweet(text):
-    # Limpeza: Remover menções, URLs, números e caracteres especiais
-    text = re.sub(r"@[A-Za-z0-9_]+", "", text)  # Remove menções
-    text = re.sub(r"http\S+|www\S+|https\S+", '', text, flags=re.MULTILINE)  # Remove URLs
-    text = re.sub(r'\d+', '', text)  # Remove números
-    text = re.sub(r'\W', ' ', text)  # Remove caracteres especiais
-    
-    # Tokenização
-    tokens = word_tokenize(text)
-    
-    # Remoção de stopwords
-    stop_words = set(stopwords.words('english'))
-    tokens = [token for token in tokens if token not in stop_words]
-    
-    return " ".join(tokens)
+def get_preprocess_tweet(lang):
+    def preprocess_tweet(text):
+        # Limpeza: Remover menções, URLs, números e caracteres especiais
+        text = re.sub(r"@[A-Za-z0-9_]+", "", text)  # Remove menções
+        text = re.sub(r"http\S+|www\S+|https\S+", '', text, flags=re.MULTILINE)  # Remove URLs
+        text = re.sub(r'\d+', '', text)  # Remove números
+        text = re.sub(r'\W', ' ', text)  # Remove caracteres especiais
+        
+        # Tokenização
+        tokens = word_tokenize(text)
+        
+        # Remoção de stopwords
+        stop_words = set(stopwords.words(lang))
+        tokens = [token for token in tokens if token not in stop_words]
+        
+        return " ".join(tokens)
+    return preprocess_tweet
 
 def vader_sentiment_result(sent):
     scores = analyzer.polarity_scores(sent)
@@ -34,11 +36,11 @@ def vader_sentiment_result(sent):
 
     return 1
 
-def analyze_sentiment(file_name):
+def analyze_sentiment(file_name, lang):
     dataset = pd.read_csv(file_name)
 
     # Pré-processar os tweets
-    dataset['Tweet'] = dataset['Tweet'].apply(preprocess_tweet)
+    dataset['Tweet'] = dataset['Tweet'].apply(get_preprocess_tweet(lang))
 
     train_set = dataset[0:5000]
     valid_set = dataset[5001:7000]
@@ -49,5 +51,5 @@ def analyze_sentiment(file_name):
     print(train_set)
     print(valid_set)
 
-analyze_sentiment("ApenasTweets.csv")
-analyze_sentiment("ApenasTweets_PT.csv")
+analyze_sentiment("ApenasTweets.csv", "english")
+analyze_sentiment("ApenasTweets_PT.csv", "portuguese")
